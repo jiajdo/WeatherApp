442ab4690e957704a841ba8cc6e97e49f1cf32a2
Jia <jovsofjdo@gmail.com>
2023-08-29 22:22:08 -0700

    Imported cors. Setup API key

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 // server/index.js
 import cors from 'cors';
 import express from 'express';
+import 'dotenv/config'
 
 const app = express();
 app.use(cors());
 
 //Set the port that you want the server to run on
 const PORT = process.env.PORT || 8080;
+const APIKEY = process.env.API_KEY
+
 
 //creates an endpoint for the route /api
+app.get('/', (req,res) =>{
+    res.send('This is the home page')
+})
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from ExpressJS' });
 });
