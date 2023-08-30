2709257d4994f966140f69895afc72056d6bb525
Jia <jovsofjdo@gmail.com>
2023-08-30 12:52:13 -0700

    Added WeatherCard component

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -4,8 +4,23 @@ import { useState } from "react";
 
 
 function Form() {
+    const [city, setCity] = useState('');
+    const [data, setData] = useState();
+
+    
   return (
-    <div><p>Form</p></div>
+    <div><p>Form</p>
+    <input
+    type='text'
+    id='city'
+    name='city'
+    placeholder='Enter a city'
+    onChange={(e) => setCity(e.target.value)}
+    ></input>
+    <button>
+    Get Weather
+    </button>
+    </div>
   )
 }
 
---
+++ client/src/components/WeatherCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from './Form'
+
+function WeatherCard() {
+  return (
+    <div>WeatherCard</div>
+  )
+}
+
+export default WeatherCard
