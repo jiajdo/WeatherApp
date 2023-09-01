ad410c32378e3739ed7bffe6ce2a4fa45d85f62c
Jia <jovsofjdo@gmail.com>
2023-09-01 07:33:28 -0700

    Created Form.css and centered WeatherCard elements

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './WeatherCard.css'
 
 
 function WeatherCard({ data, city }) {
     if (data && city) {
         return (
-            <div>
+            <div className='weatherCard'>
                 <h3>
                     <span>{data.name}, {data.sys.country}</span>
                 </h3>
