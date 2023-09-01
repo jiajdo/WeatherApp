3a4710360a095768fb31a7401c1a1fb7ffee45bf
Jia <jovsofjdo@gmail.com>
2023-09-01 07:21:12 -0700

    Added temperature and description to WeatherCard

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -5,13 +5,18 @@ function WeatherCard({ data, city }) {
     if (data && city) {
         return (
             <div>
-                <h2>
-                    <span>{data.name}</span>
-                </h2>
-                <h2>
-                    <span>{data.sys.country}</span>
-                </h2>
-
+                <h3>
+                    <span>{data.name}, {data.sys.country}</span>
+                </h3>
+                <h3>
+                    <span>{Math.round((data.main.temp-273.15) * 9/5 + 32)} Fahrenheit, {Math.round((data.main.temp-273.15))} Celsius </span>
+                </h3>
+                <h3>
+                    <span> Feels like: {Math.round((data.main.feels_like-273.15) * 9/5 + 32)} Fahrenheit, {Math.round((data.main.feels_like-273.15))} Celsius </span>
+                </h3>
+                <h3>
+                    <span>Description: {data.weather[0].description}</span>
+                </h3>
             </div>
         )
     }
